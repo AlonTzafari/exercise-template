4e8e790181c889f1ab09305c739487e7d2bc0381
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-01-19 11:53:48 +0200

    Q1

--- q1.js
+++ q1.js
@@ -1,11 +1,13 @@
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
-
+    let out;
+/**/ 
+    try {
+        out = Array.isArray(JSON.parse(input));
+    } catch(e) {
+        out = false;
+    }
 /**/
     output.innerText = out;
 }
